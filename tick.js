346ac430f1bf45202f79b7d0b0380395cfce9124
Tina Huynh <tinamchuynh@gmail.com>
2023-02-12 13:56:06 -0800

    working js clock

---
+++ tick.js
@@ -0,0 +1,22 @@
+const secondHand = document.querySelector('#seconds');
+const minuteHand = document.querySelector('#minutes');
+const hourHand = document.querySelector('#hour');
+
+function setDate() {
+	const now = new Date();
+	const seconds = now.getSeconds();
+	const secondsDegrees = ((seconds / 60) * 360) + 90;
+	secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+	const minutes = now.getMinutes();
+	const minutesDegrees = ((minutes / 60) * 360) + 90;
+	minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+	const hours = now.getHours();
+	const hoursDegrees = ((hours / 12) * 360) + 90;
+	hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+	console.log(seconds);
+}
+
+setInterval(setDate,1000);
